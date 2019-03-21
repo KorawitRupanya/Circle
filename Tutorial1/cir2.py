90cc93b18553f29b95f9813811b6da4de53c16e0
KorawitRupanya <smart1micky268@gmail.com>
2019-03-21 14:05:33 +0700

    format

--- Tutorial1/cir2.py
+++ Tutorial1/cir2.py
@@ -9,48 +9,53 @@ vys = []
 xs = []
 ys = []
 n = 10
- 
+
+
 def initialize():
     for i in range(n):
         xs.append(randint(100, SCREEN_WIDTH-100))
         ys.append(randint(100, SCREEN_HEIGHT-100))
-        vxs.append(randint(-5,5))
-        vys.append(randint(-5,5))
+        vxs.append(randint(-5, 5))
+        vys.append(randint(-5, 5))
 
 
 def move_circle(i):
 
-    global vxs,vys,xs,ys
+    global vxs, vys, xs, ys
 
     xs[i] += vxs[i]
     ys[i] += vys[i]
 
     if xs[i] > SCREEN_WIDTH - 20  \
-         or xs[i] < 20 : 
-       vxs[i] *= -1
+            or xs[i] < 20:
+        vxs[i] *= -1
     if ys[i] > SCREEN_HEIGHT - 20 \
-          or ys[i] < 20 :
+            or ys[i] < 20:
         vys[i] *= -1
+
 
 def draw_circle(i):
     arcade.draw_circle_outline(xs[i], ys[i], 20, arcade.color.BLACK)
 
+
 def on_draw(delta_time):
     arcade.start_render()
- 
+
     for i in range(n):
         move_circle(i)
         draw_circle(i)
 
+
 def main():
     initialize()
- 
+
     arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT,
                        "Circles")
     arcade.set_background_color(arcade.color.WHITE)
- 
+
     arcade.schedule(on_draw, 1 / 80)
     arcade.run()
+
 
 if __name__ == '__main__':
     main()
--- Tutorial1/cir4.py
+++ Tutorial1/cir4.py
@@ -1,6 +1,8 @@
-import arcade,math
+import arcade
+import math
 from random import randint
 from pyglet.window import key
+
 
 class Circle:
     def __init__(self, x, y, vx, vy, r=20):
@@ -9,31 +11,32 @@ class Circle:
         self.vx = vx
         self.vy = vy
         self.r = r
- 
+
     def move(self):
         self.x += self.vx
         self.y += self.vy
 
         if self.x > SCREEN_WIDTH - self.r  \
-                or self.x < self.r : 
+                or self.x < self.r:
             self.vx *= -1
         if self.y > SCREEN_HEIGHT - self.r \
-                or self.y < self.r :
+                or self.y < self.r:
             self.vy *= -1
 
     def stop(self):
         arcade.draw_circle_outline(self.x, self.y,
                                    self.r, arcade.color.WHITE)
-        
+
     def draw(self):
         arcade.draw_circle_outline(self.x, self.y,
                                    self.r, arcade.color.BLACK)
+
 
 class Player:
     def __init__(self, x, y):
         self.x = x
         self.y = y
- 
+
     def draw(self):
         arcade.draw_circle_filled(self.x, self.y,
                                   10, arcade.color.BLUE)
@@ -47,18 +50,19 @@ class Player:
             self.y += 5
         if keys[key.DOWN]:
             self.y -= 5
-    
+
     def restart(self):
-        self.x = 0 
+        self.x = 0
         self.y = 0
 
     def is_hit(self, circle):
-        a = self.x - circle.x 
+        a = self.x - circle.x
         b = self.y - circle.y
         c = math.sqrt(((a**2)+(b**2)))
         if c < 10+circle.r:
             return True
         return False
+
 
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
@@ -66,21 +70,23 @@ SCREEN_HEIGHT = 600
 circles = []
 player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
 n = 10
- 
+
 keys = key.KeyStateHandler()
+
 
 def initialize():
     for i in range(n):
         circle = Circle(randint(100, SCREEN_WIDTH-100),
                         randint(100, SCREEN_HEIGHT-100),
-                        randint(-5,5),
-                        randint(-5,5),
-                        randint(10,20))
+                        randint(-5, 5),
+                        randint(-5, 5),
+                        randint(10, 20))
         circles.append(circle)
+
 
 def on_draw(delta_time):
     arcade.start_render()
- 
+
     for c in circles:
         c.move()
         c.draw()
@@ -93,16 +99,16 @@ def on_draw(delta_time):
 
 def main():
     initialize()
- 
+
     arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT,
                        "Circles")
     arcade.set_background_color(arcade.color.WHITE)
 
     arcade.get_window().push_handlers(keys)
- 
+
     arcade.schedule(on_draw, 1 / 80)
     arcade.run()
 
+
 if __name__ == '__main__':
     main()
-
